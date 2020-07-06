10136888d4f052df6895a2c637e79a9c4f4e098d
Thomas Haslam <thomas.haslam@softwire.com>
2020-07-06 16:36:14 +0100

    part 1

--- app/src/main/java/com/example/fizzbuzz/buzzfizz.kt
+++ app/src/main/java/com/example/fizzbuzz/buzzfizz.kt
@@ -1,5 +1,17 @@
 package com.example.fizzbuzz
 
 fun main(){
-    print("hello world")
+    for (n in 1..100){
+        var s=""
+        if (n%3==0){
+            s=s.plus("Fizz")
+        }
+        if (n%5==0){
+            s=s.plus("Buzz")
+        }
+        if (s==""){
+            s=n.toString()
+        }
+        print(s.plus("\n"))
+    }
 }
